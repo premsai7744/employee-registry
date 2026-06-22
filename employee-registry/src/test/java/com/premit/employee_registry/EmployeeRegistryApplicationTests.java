23d5a3897b4386faf8b6247e03b5bc589b2216ef
premsai7744 <premsai7744@gmail.com>
2026-06-22 15:22:22 +0530

    Initial commit

---
+++ employee-registry/src/test/java/com/premit/employee_registry/EmployeeRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.premit.employee_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
